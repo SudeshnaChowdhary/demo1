f258b9aa4835bdf7035dc2d8ed255789e19c03be
sunelapa <sunelapa@DIN73003165.corp.capgemini.com>
2018-11-23 14:25:01 +0530

    first commit

---
+++ Day1-BDD/src/main/java/com/capgemini/Demo.java
@@ -0,0 +1,14 @@
+package com.capgemini;
+
+public class Demo {
+	public static void main(String args[])
+	{
+
+	System.out.println("hi");
+	System.out.println(greet());
+}
+	public static String greet()
+	{
+		return "hello";
+	}
+}
